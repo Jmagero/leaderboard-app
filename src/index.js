bea18b1ad211cefe425267067cd3a9ccf04c4f4e
Jocyline Magero <jmagero2017@gmail.com>
2021-08-04 16:59:43 -0500

    Add eventListerner to refresh button

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import '@fortawesome/fontawesome-free/js/all.js';
 import './style.css';
+import displayUsers from './getScores';
+
+const refreshBtn = document.getElementById('refresh');
+refreshBtn.addEventListener('click', (e) =>{
+    e.preventDefault()
+    displayUsers();
+});
